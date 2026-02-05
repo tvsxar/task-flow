e4b12ba009a307dca714bc341718824d24c39b7a
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-02-05 12:36:18 +0200

    feat: add LoadingSpinner component when loading is true

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,24 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import Header from "@/components/Header";
+import LoadingSpinner from "@/components/LoadingSpinner";
 import TaskInput from "@/components/TaskInput";
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useQuery } from "convex/react";
 import { LinearGradient } from 'expo-linear-gradient';
-import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { StatusBar, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
 
   const homeStyles = createHomeStyles(colors);
+
+  const tasks = useQuery(api.tasks.getTasks);
+
+  const isLoading = tasks === undefined;
+
+  if (isLoading) return <LoadingSpinner />
 
   return (
     <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
@@ -20,9 +29,10 @@ export default function Index() {
         <Header />
 
         <TaskInput />
-        <TouchableOpacity onPress={toggleDarkMode}>
-          <Text>toggle the mode</Text>
-        </TouchableOpacity>
+
+        {tasks?.map(task => (
+          <Text key={task._id}>{task.text}</Text>
+        ))}
       </SafeAreaView>
     </LinearGradient>
   );
